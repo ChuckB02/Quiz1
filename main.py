73ad80258de37e6515b26aa886b82a16f26c687c
Tobias Borges <tobias.borgesguimaraes@gmail.com>
2024-02-07 18:58:57 -0300

    Added the 2 other frames

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ class App:
         self.frameHeader = tk.Frame(self.root, bg= "#FFECB3")
         self.frameHeader.place(relx= 0.03, rely= 0.03, relwidth= 0.94, relheight= 0.17)
 
+        self.frameQuestion = tk.Frame(self.root, bg= "#FFECB3")
+        self.frameQuestion.place(relx= 0.03, rely= 0.23, relwidth= 0.94, relheight= 0.40)
+
+        self.frameButtons = tk.Frame(self.root, bg= "#FFECB3")
+        self.frameButtons.place(relx= 0.03, rely= 0.66, relwidth= 0.94, relheight= 0.31)
+        
+
 
 
 
